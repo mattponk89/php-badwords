d94c4757bcd2a5d1716ff24356ddff093a41848c
Matteo Ponchietti <matteoponchietti@gmail.com>
2020-12-22 16:24:58 +0000

    fix output pre & post

--- index.php
+++ index.php
@@ -6,6 +6,12 @@ $badword = $_GET['badword'];
 //Creo una variabile con una frase
 $paragraph = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores eos consequatur numquam dolorem expedita totam ad aut cumque ullam eum nihil, voluptatum obcaecati in. Porro vero sequi architecto sit veritatis!";
 
+echo '<h1>PRE ELABORAZIONE</h1>';
+
+echo '<p>'. $paragraph . '</p>';
+$para_len = strlen($paragraph);
+echo '<p>Numero caratteri prima: '. $para_len . '</p>';
+
 //Sostituisco la stringa amet con i tre ***
 $paragraph = str_replace($badword, "***", $paragraph);
 
@@ -15,5 +21,7 @@ $para_len = strlen($paragraph);
 ?>
 
 <!-- Stampo gli output -->
-<p>Numero di caratteri del paragrafo: <?php echo $para_len;?>
-<p>Il paragrafo è: <?php echo $paragraph;?></p>
+
+<h1>POST ELABORAZIONE</h1>
+<p>Il paragrafo è diventato: <?php echo $paragraph;?></p>
+<p>Numero di caratteri del paragrafo dopo: <?php echo $para_len;?>
